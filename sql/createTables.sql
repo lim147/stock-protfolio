a7c2513ddf2e935821fad879c879c0ab7ed761cc
joyglitch <joycesubscribe@outlook.com>
2022-08-18 01:02:11 +0000

    Clean up SQL script

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS conygre;
-
 USE conygre;
 
-CREATE TABLE IF NOT EXISTS stocks
+DROP TABLE IF EXISTS stocks; -- run if database needs a reset
+CREATE TABLE stocks
   (
      id                  INT PRIMARY KEY auto_increment,
      symbol              VARCHAR (50),
@@ -15,4 +15,4 @@ CREATE TABLE IF NOT EXISTS stocks
 
 INSERT INTO stocks VALUES (1, 'AAPL', 'Apple', 172.10, "2022-08-12 16:08:12", 5, 0);
 INSERT INTO stocks VALUES (2, 'MSFT', 'Microsoft', 291.91, "2022-07-15 13:10:15", 10, 1);
-INSERT INTO stocks VALUES (3, 'TSLA', 'Tesla', 900, "2022-06-12 10:30:20", 2, 0); 
+INSERT INTO stocks VALUES (3, 'TSLA', 'Tesla', 900, "2022-06-12 10:30:20", 2, 0);
