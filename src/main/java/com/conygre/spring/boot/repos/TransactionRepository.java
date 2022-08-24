649f30bc4c1c2ba9e7aa40c2b46e380a6e8858fd
unknown <mayjingl@outlook.com>
2022-08-24 14:15:07 +0000

    add finder methods

--- src/main/java/com/conygre/spring/boot/repos/TransactionRepository.java
+++ src/main/java/com/conygre/spring/boot/repos/TransactionRepository.java
@@ -4,8 +4,14 @@ import com.conygre.spring.boot.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-    public Iterable<Transaction> findByStockSymbol(String stockSymbol);
     public Transaction findById(int id);
+    public Collection<Transaction> findByStockSymbol(String stockSymbol);
+    public Collection<Transaction> findByType(String type);
+    public Collection<Transaction> findBySubmittedDateTimeBetween(LocalDateTime start, LocalDateTime end);
+    public Collection<Transaction> findBySubmittedPriceBetween(Double start, Double end);
 }
--- src/main/java/com/conygre/spring/boot/rest/TransactionController.java
+++ src/main/java/com/conygre/spring/boot/rest/TransactionController.java
@@ -1,10 +1,13 @@
 package com.conygre.spring.boot.rest;
 
+import com.conygre.spring.boot.entities.Stock;
 import com.conygre.spring.boot.entities.Transaction;
 import com.conygre.spring.boot.services.TransactionService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 import java.util.Collection;
 
 @RestController
@@ -20,14 +23,40 @@ public class TransactionController{
         return service.getAllTransactions();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "stock-symbol/{stock_symbol}")
-    public Iterable<Transaction> getTransactionByStockSymbol(@PathVariable("stock_symbol") String stockSymbol) {
-        return service.getTransactionByStockSymbol(stockSymbol);
-    }
-
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public Transaction getTransactionById(@PathVariable("id") int id) {
         return service.getTransactionById(id);
+    }
+
+    // Filter transaction based on conditions:
+    @RequestMapping(method = RequestMethod.GET, value = "stock-symbol/{stock_symbol}")
+    public Collection<Transaction> getTransactionsByStockSymbol(@PathVariable("stock_symbol") String stockSymbol) {
+        return service.getTransactionsByStockSymbol(stockSymbol);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "type/{type}")
+    public Collection<Transaction> getTransactionsByType(@PathVariable("type") String type) {
+        return service.getTransactionsByType(type);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "submitted-date-between/{startDate}/{endDate}")
+    public Collection<Transaction> getTransactionsBetweenDate(
+            @PathVariable("startDate") String start, @PathVariable("endDate") String end) {
+        LocalDate startDate = LocalDate.parse(start);
+        LocalDate endDate = LocalDate.parse(end);
+        return service.getTransactionsBetweenDate(startDate, endDate);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "submitted-price-between/{startPrice}/{endPrice}")
+    public Collection<Transaction> getTransactionsBetweenPrice(
+            @PathVariable("startPrice") Double start, @PathVariable("endPrice") Double end) {
+        return service.getTransactionsBetweenPrice(start, end);
+    }
+
+    // Add transaction:
+    @RequestMapping(method = RequestMethod.POST)
+    public void addTransaction(@RequestBody Transaction transaction) {
+        service.addTransaction(transaction);
     }
 
 
