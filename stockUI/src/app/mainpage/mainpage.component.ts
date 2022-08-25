56abdc1ddaf76c43cb126dc5ff69599fe89c9698
joyglitch <joycesubscribe@outlook.com>
2022-08-25 18:02:30 +0000

    Transfer over to Transaction page

--- stockUI/src/app/mainpage/mainpage.component.ts
+++ stockUI/src/app/mainpage/mainpage.component.ts
@@ -1,40 +1,13 @@
-import { Component} from '@angular/core';
-import { Observable } from 'rxjs';
-import { Transaction } from '../transaction';
-import { TransactionService } from '../transaction.service';
-import { NgForm } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
   styleUrls: ['./mainpage.component.css']
 })
 
-export class MainpageComponent{
-  transactions!: Array<Transaction>;
-  searchId : number = 1;
+export class MainpageComponent implements OnInit {
+  constructor() { }
 
-  constructor(private transactionService: TransactionService) { }
-
-  ngOnInit() {
-    this.getAllTransactions();
+  ngOnInit(): void {
   }
-
-  getAllTransactions(){
-    this.transactionService.getAllTransactions().subscribe({
-      next: (data:any) => this.transactions = data,
-      error: (_:any)  => console.log("Error")
-    });
-  }
-
-  // Add transaction to the transactions list
-  searchTransactionById(form: NgForm){
-    console.log(this.searchId);
-    this.transactions = [];
-    this.transactionService.getTransactionById(this.searchId).subscribe({
-      next: (data:any) => this.transactions.push(data),
-      error: (_:any)  => console.log("Error")
-    });
-  }
-
 }
--- stockUI/src/app/searchpage/searchpage.component.ts
+++ stockUI/src/app/searchpage/searchpage.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Transaction } from '../transaction';
+import { TransactionService } from '../transaction.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-searchpage',
   templateUrl: './searchpage.component.html',
   styleUrls: ['./searchpage.component.css']
 })
-export class SearchpageComponent implements OnInit {
+export class SearchpageComponent{
+  transactions!: Array<Transaction>;
+  searchId : number = 1;
 
-  constructor() { }
+  constructor(private transactionService: TransactionService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getAllTransactions();
   }
 
+  getAllTransactions(){
+    this.transactionService.getAllTransactions().subscribe({
+      next: (data:any) => this.transactions = data,
+      error: (_:any)  => console.log("Error")
+    });
+  }
+
+  // Add transaction to the transactions list
+  searchTransactionById(form: NgForm){
+    console.log(this.searchId);
+    this.transactions = [];
+    this.transactionService.getTransactionById(this.searchId).subscribe({
+      next: (data:any) => this.transactions.push(data),
+      error: (_:any)  => console.log("Error")
+    });
+  }
 }
