9f275c133e3b8aac81d2a5331e682eb2b2f39340
unknown <mayjingl@outlook.com>
2022-08-25 15:49:24 +0000

    add searchById

--- stockUI/src/app/mainpage/mainpage.component.ts
+++ stockUI/src/app/mainpage/mainpage.component.ts
@@ -2,6 +2,7 @@ import { Component} from '@angular/core';
 import { Observable } from 'rxjs';
 import { Transaction } from '../transaction';
 import { TransactionService } from '../transaction.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-mainpage',
@@ -10,10 +11,26 @@ import { TransactionService } from '../transaction.service';
 })
 
 export class MainpageComponent{
-  transactions: Observable<Array<Transaction>>;
-  constructor(transactionService: TransactionService) { 
-    this.transactions = transactionService.getAllTransactions();
+  transactions!: Observable<Array<Transaction>>;
+  transaction!: Transaction;
+  searchId : number = 1;
 
+  constructor(private transactionService: TransactionService) { 
+    this.transactions = this.transactionService.getAllTransactions();
+  }
+
+  ngOnInit() {
+    //this.transactions = this.transactionService.getAllTransactions();
+  }
+ 
+
+  // Add transaction to the transactions list
+  searchTransactionById(form: NgForm){
+    console.log(this.searchId);
+    this.transactionService.getTransactionById(this.searchId).subscribe({
+      next: (data:any) => this.transaction = data,
+      error: (_:any)  => console.log("Error")
+    });
   }
 
 }
