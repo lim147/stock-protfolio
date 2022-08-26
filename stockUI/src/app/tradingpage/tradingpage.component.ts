afa40bc1f0ef3af8adbd7df20e8932e717e43bb2
unknown <mayjingl@outlook.com>
2022-08-26 00:08:58 +0000

    move buy/sell stock to trading page

--- stockUI/src/app/tradingpage/tradingpage.component.ts
+++ stockUI/src/app/tradingpage/tradingpage.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Stock } from '../stock';
+import { StockService } from '../stock.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-tradingpage',
   templateUrl: './tradingpage.component.html',
   styleUrls: ['./tradingpage.component.css']
 })
-export class TradingpageComponent implements OnInit {
+export class TradingpageComponent {
+  stocks! : Array<Stock>;
+  
+  stockToBuyName! : string;
+  stockToBuyQty! : number;
+  stockToBuySymbol! : string;
 
-  constructor() { }
+  stockToSellName! : string;
+  stockToSellQty! : number;
+  stockToSellSymbol! : string;
+
+  constructor(private stockService : StockService) { }
 
   ngOnInit(): void {
   }
+
+  buyStock(form: NgForm){
+    this.stockService.buyStock(this.stockToBuySymbol, this.stockToBuyName, this.stockToBuyQty).subscribe({
+      next: (data:any) => this.stocks.push(data),
+      error: (_:any)  => console.log("Error")
+    });
+  }
+
+  sellStock(form: NgForm){
+    this.stockService.sellStock(this.stockToSellSymbol, this.stockToSellName, this.stockToSellQty).subscribe({
+      next: (data:any) => this.stocks.push(data),
+      error: (_:any)  => console.log("Error")
+    });
+  }
+
 
 }
