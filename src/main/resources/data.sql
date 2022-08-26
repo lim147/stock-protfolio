c991fae504fd8f094869445d06b787b416028389
unknown <mayjingl@outlook.com>
2022-08-26 03:27:35 +0000

    clean data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO stocks VALUES ('AAPL', 'Apple', 5);
+INSERT INTO stocks VALUES ('AAPL', 'Apple', 12);
 INSERT INTO stocks VALUES ('MSFT', 'Microsoft', 10);
 INSERT INTO stocks VALUES ('TSLA', 'Tesla', 2);
 
-INSERT INTO transactions VALUES (1, 'AAPL', "2022-08-12 16:08:12", 172.10,  5, 'BUY');
+INSERT INTO transactions VALUES (1, 'AAPL', "2022-08-12 16:08:12", 172.10,  15, 'BUY');
 INSERT INTO transactions VALUES (2, 'MSFT', "2022-07-15 13:10:15", 291.91, 10, 'SELL');
 INSERT INTO transactions VALUES (3, 'TSLA', "2022-06-12 10:30:20", 900, 2, 'BUY');
 INSERT INTO transactions VALUES (4, 'AAPL', "2022-06-12 10:30:20", 160, 3, 'BUY');
